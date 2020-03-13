443037de785e1e425d2b77bc469277512762b2a2
julianamokrejs <ju.mokrejs@gmail.com>
2020-03-13 17:59:32 -0300

    Updates

---
+++ precache-manifest.7f6fd66dfb5426485da6ab5f3136a92b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b1251c7d609f22d728b10251f3be44",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "101905a483b633f0a3ff",
+    "url": "/react-crud/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ba6b0da60077e89f3915",
+    "url": "/react-crud/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "101905a483b633f0a3ff",
+    "url": "/react-crud/static/js/2.7f421a8e.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/react-crud/static/js/2.7f421a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba6b0da60077e89f3915",
+    "url": "/react-crud/static/js/main.8022219d.chunk.js"
+  },
+  {
+    "revision": "da84b4dda3923e56a26d",
+    "url": "/react-crud/static/js/runtime-main.13c5cd7d.js"
+  }
+]);
